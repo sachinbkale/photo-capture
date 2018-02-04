061579811da31efe4b71202433eec87817d83326
sachinbkale <sachinbkale@users.noreply.github.com>
2018-02-04 11:48:57 -0800

    Add files via upload

--- src/main/java/com/cronos/posidon/controller/GlobalExceptionHandler.java
+++ src/main/java/com/cronos/posidon/controller/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.cronos.posidon.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MultipartException;
@@ -7,13 +9,14 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
     //https://jira.spring.io/browse/SPR-14651
     //4.3.5 supports RedirectAttributes redirectAttributes
     @ExceptionHandler(MultipartException.class)
     public String handleError1(MultipartException e, RedirectAttributes redirectAttributes) {
 
-        redirectAttributes.addFlashAttribute("message", e.getCause().getMessage());
+        redirectAttributes.addFlashAttribute("message", e.getCause().getMessage());       
+        logger.error(e.getCause().getMessage());
         return "redirect:/uploadStatus";
 
     }
--- src/main/java/com/cronos/posidon/controller/UploadController.java
+++ src/main/java/com/cronos/posidon/controller/UploadController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,7 +19,9 @@ import java.nio.file.Paths;
 public class UploadController {
 
     //Save the uploaded file to this folder
-    private static String UPLOADED_FOLDER = "/images-upload";
+    //private static String UPLOADED_FOLDER = "/Users/namita/image-upload/";
+	private static String UPLOADED_FOLDER = "/images-upload/";
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
     @GetMapping("/")
     public String index() {
@@ -33,20 +38,28 @@ public class UploadController {
 
         if (file.isEmpty()) {
             redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            logger.error("ERROR : Please select a file to upload");
             return "redirect:uploadStatus";
         }
 
         try {
+
             // Get the file and save it somewhere
             byte[] bytes = file.getBytes();
             Path path = Paths.get(UPLOADED_FOLDER + "/"+file.getOriginalFilename());
-            Files.write(path, bytes);
 
+            logger.info("Uplpading | Folder Path=" + path.getParent());
+
+            Files.write(path, bytes);            
+            
             redirectAttributes.addFlashAttribute("message",
-                    "You successfully uploaded '" + file.getOriginalFilename() +"'");
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
 
+            logger.info("Uploaded  Successfulle | File Path=" + path.getParent() +"/" + path.getFileName());
+            
         } catch (IOException e) {
             e.printStackTrace();
+            logger.error("Error uploading file", e);
         }
 
         return "redirect:/uploadStatus";
@@ -56,4 +69,5 @@ public class UploadController {
     public String uploadStatus() {
         return "uploadStatus";
     }
+
 }
